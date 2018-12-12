6aec7ac9370f1a1aee45d0e5595824b1b2f2eb57
bobmadoshi <ppsteve@gmail.com>
2018-12-12 09:40:35 -0600

    updating settings

--- YAPO/settings.py
+++ YAPO/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
     # 'selectable',
     'videos.apps.VideosConfig',
     'mptt',
