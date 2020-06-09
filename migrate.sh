1f9b4f1c583debf489c204062e97febba646301a
cooperdk <564150+cooperdk@users.noreply.github.com>
2020-06-09 12:48:12 +0200

    YAPO now listens on all interfaces (test for Docker)

---
+++ migrate.sh
@@ -0,0 +1,4 @@
+#!bin/sh
+
+python manage.py makemigrations
+python manage.py migrate
--- yapo.sh
+++ yapo.sh
@@ -1,4 +1,4 @@
 #!bin/sh
 
-python ./manage.py runserver 127.0.0.1:8000
+python ./manage.py runserver 0.0.0.0:8000
 
